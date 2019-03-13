c6b1f8ebe835d4ef99e3b715af1906302594b9d6
Harriet Ellis <harriet.p.ellis@gmail.com>
2019-03-13 14:02:32 +0100

    reading dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 
+
+posts = [
+{
+'author': 'Harriet',
+'title': 'Blog Post 1',
+'content': 'First post content',
+'date_posted': 'March 13, 2019'
+},
+{
+'author': 'Harriet',
+'title': 'Blog Post 2',
+'content': 'Second post content',
+'date_posted': 'March 15, 2019'
+}
+
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+    'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
