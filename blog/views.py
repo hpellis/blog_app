8501fd5eae9bd5551dd7c15425b01ad4b29cfbb8
Harriet Ellis <harriet.p.ellis@gmail.com>
2019-03-13 16:02:40 +0100

    adding registration page

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-
-posts = [
-{
-'author': 'Harriet',
-'title': 'Blog Post 1',
-'content': 'First post content',
-'date_posted': 'March 13, 2019'
-},
-{
-'author': 'Harriet',
-'title': 'Blog Post 2',
-'content': 'Second post content',
-'date_posted': 'March 15, 2019'
-}
-
-]
-
 def home(request):
     context = {
     'posts':Post.objects.all()
