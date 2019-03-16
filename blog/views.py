39993031ca1131e04c908851ab9084f4fbce090c
hpellis <harriet.p.ellis@gmail.com>
2019-03-16 16:57:45 +0000

    adding pages for blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -9,11 +9,19 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    
+
+
+
 
 
 def about(request):
