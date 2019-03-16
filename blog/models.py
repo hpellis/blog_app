0ded8788647e60ecc6b0adffd8f4f8f4b242668a
hpellis <harriet.p.ellis@gmail.com>
2019-03-16 17:23:06 +0000

    completed update view

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-# Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
