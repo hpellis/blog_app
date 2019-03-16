565d1f364752ac7ecf5cf2e36d0d8dc6526e8b79
hpellis <harriet.p.ellis@gmail.com>
2019-03-16 16:47:23 +0000

    adding class-based view for posts

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
+
 
 def home(request):
     context = {
-    'posts':Post.objects.all()
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
